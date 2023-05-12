b39e2a3ef7b3ed40b22144ecfda75b352ac6752a
erickim0223 <erickim0223@yahoo.com>
2023-05-11 23:06:26 -0400

    change names

--- learncsharp/helloworld.cs
+++ learncsharp/helloworld.cs
@@ -7,8 +7,8 @@ namespace Helloworld
         public static void helloworld() {
             System.Console.WriteLine(add(1,3));
         }
-        public static int add(int i, int i2) {
-            return i + i2;
+        public static int add(int number_one, int number_two) {
+            return number_one + number_two;
         }
     }
 }
