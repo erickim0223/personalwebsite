e2d06c52c23c267d04287f919af231d5c3e8079d
erickim0223 <erickim0223@yahoo.com>
2023-05-18 19:19:26 -0400

    string interpolation

--- learncsharp/helloworld.cs
+++ learncsharp/helloworld.cs
@@ -4,22 +4,10 @@ namespace Helloworld
 {
     class Hello {
         static void Main(string[] args) {
-            helloworld();
-            // Console.WriteLine(Math.Max(1, 5));
-        }
-        public static void helloworld() {
-            // Console.WriteLine("Enter your name: ");
-            // string name = Console.ReadLine();
-            // Console.WriteLine("Hi, " + name);
-            Console.WriteLine(add(2,3));
-            string mystr = "Hello world!";
-            for (int i = 0; i < mystr.Length; i++) {
-                Console.Write(mystr[i]);
-            }
-        }
-        //comment add function
-        public static int add(int number_one, int number_two) {
-            return number_one + number_two;
+            string firstName = "Eric";
+            string lastName = "Kim";
+            string name = $"My full name is: {firstName} {lastName}";
+            Console.WriteLine(name);
         }
     }
 }
