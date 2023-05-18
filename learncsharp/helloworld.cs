c8c654188874b9351de3d4b72d0ef71a11701bf6
erickim0223 <erickim0223@yahoo.com>
2023-05-17 23:20:27 -0400

    c# strings

--- learncsharp/helloworld.cs
+++ learncsharp/helloworld.cs
@@ -5,13 +5,17 @@ namespace Helloworld
     class Hello {
         static void Main(string[] args) {
             helloworld();
-            Console.WriteLine(Math.Max(1, 5));
+            // Console.WriteLine(Math.Max(1, 5));
         }
         public static void helloworld() {
             // Console.WriteLine("Enter your name: ");
             // string name = Console.ReadLine();
             // Console.WriteLine("Hi, " + name);
             Console.WriteLine(add(2,3));
+            string mystr = "Hello world!";
+            for (int i = 0; i < mystr.Length; i++) {
+                Console.Write(mystr[i]);
+            }
         }
         //comment add function
         public static int add(int number_one, int number_two) {
