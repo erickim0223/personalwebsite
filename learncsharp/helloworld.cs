7a6925fd761f619cd535424e235be0b9184a4c29
erickim0223 <erickim0223@yahoo.com>
2023-05-20 18:29:00 -0400

    Person object

--- learncsharp/helloworld.cs
+++ learncsharp/helloworld.cs
@@ -2,11 +2,15 @@ using System;
 
 namespace Helloworld
 {
+    class Person {
+        public string Name  // property
+        { get; set; }
+    }
     class Hello {
         static void Main(string[] args) {
-            int test = 0;
-            string res = (test == 0) ? "True" : "False";
-            Console.WriteLine(res);
+            Person mypers = new Person();
+            mypers.Name = "Name";
+            Console.WriteLine(mypers.Name);
         }
     }
 }
